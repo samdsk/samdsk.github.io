4927728013b012ffbae8fcb9ad2738d643cdc6fa
Sam. K. <sam.ds@live.com>
2023-09-26 11:49:54 +0200

    refactoring nav

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <Nav/>
     <main className="flex flex-col min-h-screen items-center p-1">
       <HomeSection/>
-      <section id="aboutMe"  className=" bg-slate-950 min-h-screen min-w-full"> About me </section>
+      <section id="about"  className=" bg-slate-950 min-h-screen min-w-full"> About me </section>
       <section id="projects" className=" bg-zinc-800 min-h-screen min-w-full"> Projects </section>
       <section id="contacts" className=" bg-gray-800 min-h-screen min-w-full"> Contacts </section>
     </main>    
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,32 +2,24 @@
 import React from 'react';
 import { Link } from "react-scroll";
 
+function ActiveNavItem({name,target}){
+    return (
+        <li className="nav-item">
+            <Link activeClass="active" to={target} spy={true} smooth={true} offset={-70} duration={500}>
+                {name}
+            </Link>
+        </li>
+    )
+}
+
 
 export default function Nav() {
     return (
-        <>
-            <ul className="flex flex-row sticky top-0 z-50 p-2 justify-end pr-10" id="nav-bar">
-                <li className="nav-item">
-                    <Link activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={500}>
-                        Home
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link activeClass="active" to="aboutMe" spy={true} smooth={true} offset={-70} duration={500}>
-                        About me
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-70} duration={500}>
-                        Projects
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link activeClass="active" to="contacts" spy={true} smooth={true} offset={-70} duration={500}>
-                        Contacts
-                    </Link>
-                </li>
-            </ul>
-        </>
+        <ul className="flex flex-row sticky top-0 z-50 p-2 justify-end pr-10" id="nav-bar">
+            <ActiveNavItem name="Home" target="home"/>
+            <ActiveNavItem name="About" target="about"/>
+            <ActiveNavItem name="Projects" target="projects"/>
+            <ActiveNavItem name="Contact" target="contacts"/>
+        </ul>
     )
 }
