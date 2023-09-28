51b744de6f5d039dfb18d6cc51b8981bcd104a18
Sam. K. <sam.ds@live.com>
2023-09-28 12:08:01 +0200

    added logo image with navigation to top

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,4 +1,14 @@
+import ExportedImage from "next-image-export-optimizer";
+import logo from "../images/logo.png";
+import { Link } from "react-scroll";
+
 export default function Logo(){
-    return (<h1>Sam. K.</h1>)
+    return (
+        <div className="relative group w-10 h-10 flex-wrap justify-center items-center hover:outline-offset-1 hover:outline-cyan-300 hover:outline outline-2   bg-white rounded-full" >
+            <Link to="home" spy={true} smooth={true} offset={-80} duration={500}>
+                <ExportedImage className="relative left-[7px] top-[2px]" src={logo} alt="logo" width={25} height={25}/>
+            </Link>
+        </div>
+    )
     
 }
