dcd6d2bebb8d9130c34354ba0f67114dcf7b0aed
Sam. K. <sam.ds@live.com>
2023-09-28 11:01:48 +0200

    changed bg colors

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -9,11 +9,11 @@ export default function HomeSection() {
         <>
             <section id="home" className="h-screen w-screen flex flex-col items-center">
                 <div className="flex flex-col justify-center mx-auto m-auto w-10/12">
-                    <div className='backdrop-blur-sm bg-white/30 rounded-lg bg-slate-500 p-5'>
+                    <div className='p-5'>
                         <div className="my-2 ">
-                            <h4 className="toUpAnimation anim-delay-200ms text-sm text-blue-800">Hello World! I am</h4>
-                            <h1 className="toUpAnimation anim-delay-500ms text-4xl w-full sm:text-5xl md:text-6xl">Sam****. K*****.</h1>
-                            <h4 className="toUpAnimation anim-delay-700ms text-xl sm:text-2xl md:text-3xl text-blue-800 w-auto ">I love exploring 🔍 and learning 📖 new things</h4>
+                            <h4 className="toUpAnimation anim-delay-200ms text-sm text-blue-500">Hello World! I am</h4>
+                            <h1 className="toUpAnimation anim-delay-500ms text-4xl font-sans  font-bold w-full sm:text-5xl md:text-6xl">Sam****. K*****.</h1>
+                            <h4 className="toUpAnimation anim-delay-700ms text-xl font-bold sm:text-2xl md:text-3xl text-blue-800 w-auto ">I love exploring 🔍 and learning 📖 new things</h4>
                         </div>
                         <div className="text-sm toUpAnimation anim-delay-1000ms mt-2">
                             <p className="w-auto sm:w-3/4 ">
@@ -27,8 +27,8 @@ export default function HomeSection() {
                         </div>
                     </div>
                     <div>
-                        <div className='p-2 mt-2 rounded-md border w-fit'>
-                            <ActiveNavItem name="Contact" target="contacts"/>
+                        <div className='hover:bg-slate-800 p-2 mt-2 ml-5 rounded-md border w-fit'>
+                            <ActiveNavItem  name="Contact" target="contacts"/>
                         </div>
                     </div>
                 </div>
