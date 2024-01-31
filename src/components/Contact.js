1f977ced9a5e9ec58e9b5b2f1b44e9f27adc617a
samdsk <sam.ds@live.com>
2024-01-31 09:43:23 +0100

    contact

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <>
+            <section id="contacts" className="min-h-screen mb-10 w-screen flex flex-col items-center">
+                <div id="mail" >
+                    <h2 className=' text-4xl font-sans font-bold'>Get In Touch</h2>
+                    <p>Feel free to mail whether you have a question or just want to say hi, I'll try my best to get back to you.</p>
+
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Contact
