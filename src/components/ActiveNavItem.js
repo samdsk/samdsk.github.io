70f0ec9e0f7b4df4456530974010fbb4c5965b7e
Sam. K. <sam.ds@live.com>
2024-05-29 13:57:40 +0200

    added custom cursor

--- src/components/ActiveNavItem.js
+++ src/components/ActiveNavItem.js
@@ -1,11 +1,19 @@
 import { Link } from "react-scroll";
 
-export default function ActiveNavItem({name,target}){
-    return (
-        <div className=" font-sans font-bold text-gray-400 hover:text-white hover:transition-colors duration-500">
-            <Link className="px-2" activeClass="active" to={target} spy={true} smooth={true} offset={-90} duration={500}>
-                {name}
-            </Link>
-        </div>
-    )
+export default function ActiveNavItem({ name, target }) {
+  return (
+    <div className=" font-sans font-bold text-gray-400 hover:text-white hover:transition-colors duration-500">
+      <Link
+        className="px-2 cursor-pointer"
+        activeClass="active"
+        to={target}
+        spy={true}
+        smooth={true}
+        offset={-90}
+        duration={500}
+      >
+        {name}
+      </Link>
+    </div>
+  );
 }
